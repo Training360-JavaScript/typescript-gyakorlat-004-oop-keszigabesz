67d5bf75a3a58ced3dccde5df74328c4c6a2fd12
keszigabesz <bajo.gabor@gmail.com>
2022-01-06 15:30:19 +0100

    kész

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,5 @@
+import { TransformerHero } from './transformer';
+import { HumanHero } from './hero';
 // Importáld be a HumanHero és TransformerHero osztályokat.
 
 
@@ -8,7 +10,32 @@
  * Hozz létre benne 3 HumanHero példányt, szabadon választott adatokkal.
  * @var {HumanHero[]} humans
  */
-export const humans: any[] = [];
+export const humans: HumanHero[] = [
+    {
+        id: 12,
+        name: 'Gizi',
+        photo: 'library',
+        sex: 'female',
+        age: 39,
+        health: 100,
+    },
+    {
+        id: 13,
+        name: 'Marci',
+        photo: 'library',
+        sex: 'male',
+        age: 29,
+        health: 90,
+    },
+    {
+        id: 14,
+        name: 'Jolán',
+        photo: 'library',
+        sex: 'female',
+        age: 69,
+        health: 120,
+    },
+];
 
 /**
  * Állítsd be helyesen a változót!
@@ -17,4 +44,29 @@ export const humans: any[] = [];
  * Hozz létre benne 3 TransformerHero példányt, szabadon választott adatokkal.
  * @var {TransformerHero[]} humans
  */
-export const transformers: any[] = [];
+export const transformers: TransformerHero[] = [
+    {
+        id: 1,
+        name: 'Lada',
+        photo: 'library',
+        wings: 2,
+        wheels: 4,
+        clan: 'ussr',
+    },
+    {
+        id: 2,
+        name: 'Trabant',
+        photo: 'library',
+        wings: 4,
+        wheels: 8,
+        clan: 'ddr',
+    },
+    {
+        id: 3,
+        name: 'Skoda',
+        photo: 'library',
+        wings: 45,
+        wheels: 14,
+        clan: 'czech',
+    },
+];
